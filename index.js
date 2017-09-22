234ee09975cbf99155d2e515b4e4f7c2025a6ddd
NadiykaS <nadijka2@gmail.com>
2017-09-22 13:49:07 +0300

    Add text: Удавсь на всеє зле проворний...

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html; charset=utf-8');
-    res.end(`Троянців насажавши повні,<br>
+    res.end(`Удавсь на всеє зле проворний,<br>
+    Завзятіший од всіх бурлак.<br><br>
+    Троянців насажавши повні,<br>
     І куди очі почухрав.<br><br>
     Но зла Юнона, суча дочка,<br>
     Розкудкудакалась, як квочка, — `);
