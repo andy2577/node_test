f54c3e0ee1cb0a2041d14833ae74f1534687c970
irrino4ka <ir.rino4ka@gmail.com>
2017-09-22 13:42:19 +0300

    Ira's rows

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html; charset=utf-8');
-    res.end(`Троянців насажавши повні,<br>
+    res.end(`Но греки, як спаливши Трою,<br/>
+	Зробили з неї скирту гною,<br/>
+	Троянців насажавши повні,<br>
     І куди очі почухрав.<br><br>
     Но зла Юнона, суча дочка,<br>
     Розкудкудакалась, як квочка, — `);
