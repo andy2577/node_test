43667ce7af75d123da5c56b0cf2cc746b334f796
Andriy <andy25771@gmail.com>
2017-09-22 12:27:14 +0300

    fixed coma and added space

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html; charset=utf-8');
-    res.end(`Троянців насажавши повні,,<br>
+    res.end(`Троянців насажавши повні,<br><br>
     І куди очі почухрав.<br>
     Но зла Юнона, суча дочка,<br>
     Розкудкудакалась, як квочка, — `);
